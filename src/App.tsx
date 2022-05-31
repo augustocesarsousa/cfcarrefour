73eba7dbfd5c9473f669500f1e95158d8eb27dd3
Augusto Cesar <acs-03@hotmail.com>
2022-05-31 18:21:36 -0300

    Get products

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Product } from './types/product';
 import { Sellers } from './types/seller';
 
 function App() {
   const [sellers, setSellers] = useState<Sellers[]>();
+  const [products, setProducts] = useState<Product[]>();
 
   useEffect(() => {
     axios
-      .get('/api/checkout/pub/regions?country=BRA&postalCode=07914010')
+      .get('/api/checkout/pub/regions?country=BRA&postalCode=17560246')
       .then((response) => {
         setSellers(response.data);
-        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const sellerId = sellers?.map((obj) => obj.sellers[2].id);
+
+  useEffect(() => {
+    axios
+      .get('/api/catalog_system/pub/products/search?fq=' + sellerId)
+      .then((response) => {
+        setProducts(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -21,7 +35,11 @@ function App() {
   return (
     <div className="App">
       <h1>Compra Fácil Carrefour</h1>
-      {sellers?.map((obj) => obj.sellers.map((seller) => <p key={seller.id}>{seller.name}</p>))}
+      {products?.map((product) => (
+        <p key={product.productId}>
+          {product.productId} - {product.productName}
+        </p>
+      ))}
     </div>
   );
 }
